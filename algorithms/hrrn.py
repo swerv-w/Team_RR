299b3c9bdc0422498770879a588593e7bb6fdf2b
macos-kw <macos.kw@gmail.com>
2026-04-18 12:33:59 +0900

    반환값 통일

--- algorithms/hrrn.py
+++ algorithms/hrrn.py
@@ -1,3 +1,7 @@
 # fcfs.py / spn.py / hrrn.py (비선점)
 def schedule(processes: list[Process]) -> tuple[list[Process], list[tuple]]:
-    pass
+    # Gantt chart 데이터
+    gantt = [("P1", 0, 3), ("P2", 3, 7), ...]
+
+    # 반환
+    return processes, gantt
--- algorithms/srtn.py
+++ algorithms/srtn.py
@@ -1,3 +1,7 @@
 # srtn.py (선점)
 def schedule(processes: list[Process]) -> tuple[list[Process], list[tuple]]:
-    pass
+    # Gantt chart 데이터
+    gantt = [("P1", 0, 3), ("P2", 3, 7), ...]
+
+    # 반환
+    return processes, gantt
