949b83b7013c5e2365c0529abdf80c30091dfb18
pangel <tjdgus751803@gmail.com>
2026-05-01 01:50:16 +0900

    ATS 알고리즘 v4

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,3 +3,4 @@ from algorithms.rr import schedule as rr
 from algorithms.spn import schedule as spn
 from algorithms.srtn import schedule as srtn
 from algorithms.hrrn import schedule as hrrn
+from algorithms.ats import schedule as ats
